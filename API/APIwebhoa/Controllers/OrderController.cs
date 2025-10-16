cb29a9cfa8285e7d8fee846d66f72310799e0906
USER <USER@DESKTOP-LFUMQJ9>
2025-10-17 01:47:22 +0700

    API của Order

---
+++ API/APIwebhoa/Controllers/OrderController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APIwebhoa.Data;
+using APIwebhoa.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace APIwebhoa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly DBContext _context;
+
+        public OrderController(DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Order
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
+        {
+            return await _context.tblOrder.ToListAsync();
+        }
+
+        // GET: api/Order/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrder(int id)
+        {
+            var order = await _context.tblOrder.FindAsync(id);
+            if (order == null)
+                return NotFound();
+            return order;
+        }
+
+        // POST: api/Order
+        [HttpPost]
+        public async Task<ActionResult<Order>> PostOrder(Order order)
+        {
+            _context.tblOrder.Add(order);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetOrder), new { id = order.PK_iOrderID }, order);
+        }
+
+        // PUT: api/Order/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrder(int id, Order order)
+        {
+            if (id != order.PK_iOrderID)
+                return BadRequest();
+
+            _context.Entry(order).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        // DELETE: api/Order/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(int id)
+        {
+            var order = await _context.tblOrder.FindAsync(id);
+            if (order == null)
+                return NotFound();
+
+            _context.tblOrder.Remove(order);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
--- API/APIwebhoa/Data/DBContext.cs
+++ API/APIwebhoa/Data/DBContext.cs
@@ -17,9 +17,12 @@ namespace APIwebhoa.Data
         public DbSet<Models.KhuyenMai> KhuyenMais { get; set; }
         public DbSet<DanhGia> DanhGias { get; set; }
         public DbSet<MauSP> MauSPs { get; set; }
-        
+        public DbSet<Order> tblOrder { get; set; }
+
+
         public DbSet<MaterSize> MaterSizes { get; set; }
 
+      
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
